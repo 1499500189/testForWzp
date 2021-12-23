5b21f05762e8244bbda705a1c3a1e97d873c9f52
14991500189 <1499500189@qq.com>
2021-12-23 16:35:08 +0800

    wzp .. 3

---
+++ Disgn/cigilibi/src/main/java/com/wzp/cigilibi/demo/test/test.java
@@ -0,0 +1,30 @@
+package com.wzp.cigilibi.demo.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+/**
+ * @author
+ * @date 2021 年 12 月 23 日
+ */
+public class test {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public  void testtt(){
+    }
+
+    public static void main(String[] args) {
+        test test = new test();
+        Boolean delete = test.redisTemplate.delete(2);
+        System.out.println(delete);
+    }
+    public  void  testk(){
+        System.out.println("解决debug源码");
+        System.out.println("使用SpelExpressionParser");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
--- Disgn/designPattern/src/interpteter/test.java
+++ Disgn/designPattern/src/interpteter/test.java
@@ -1,12 +1,11 @@
+package interpteter;
+
 /**
  * @author
- * @date 2021 年 12 月 20 日
+ * @date 2021 年 12 月 23 日
  */
-
 public class test {
-    //冒泡排序
     public static void main(String[] args) {
 
     }
-
 }
