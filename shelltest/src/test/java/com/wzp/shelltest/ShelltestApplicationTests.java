02671873b257decadf7573a2ef8ba6d198edf3d2
14991500189 <1499500189@qq.com>
2022-04-02 11:39:29 +0800

    shell学习

---
+++ shelltest/src/test/java/com/wzp/shelltest/ShelltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzp.shelltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
